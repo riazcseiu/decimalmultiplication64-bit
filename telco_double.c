d26026fce2c770d546cb37d0ea854861f3b79aaa
riazcseiu <41711377+riazcseiu@users.noreply.github.com>
2020-09-15 03:41:03 +0900

    Add files via upload

--- telco_double.c
+++ telco_double.c
@@ -1,4 +1,4 @@
-#define DECNUMDIGITS 34
+//#define DECNUMDIGITS 34
 #define  MAXFILE     255           // maximum filename length
 #define  DECNUMDIGITS 31           // maximum precision
 #include "decNumber.h"             // base number library
@@ -25,19 +25,19 @@
 /* ------------------------------------------------------------------ */
 int main(int argc, char *argv[]) {
 
-  decNumber sumT, sumB, sumD;      // sums
+ //# decNumber sumT, sumB, sumD;      // sums
   decDouble d_sumT, d_sumB, d_sumD;      // sums
 
-  decNumber n;                     // number from file
-  decNumber p;                     // price
-  decNumber b;                     // base tax
-  decNumber d;                     // distance tax
-  decNumber t;                     // total price
+  //#decNumber n;                     // number from file
+  //#decNumber p;                     // price
+  //#decNumber b;                     // base tax
+ //# decNumber d;                     // distance tax
+ //# decNumber t;                     // total price
   decDouble d_n,d_p,d_b,d_d,d_t;   //all value in double format
  
-  decNumber baserate, distrate;    // call rates
-  decNumber basetax, disttax;      // tax rates
-  decNumber mtwo;                  // constant -2
+ //# decNumber baserate, distrate;    // call rates
+ //# decNumber basetax, disttax;      // tax rates
+ //# decNumber mtwo;                  // constant -2
 
   decDouble  d_baserate, d_distrate, d_basetax, d_disttax, d_mtwo; // all value in double format
 
@@ -46,6 +46,7 @@ int main(int argc, char *argv[]) {
   decContext set;                  // working context
 
   decimal64 in64;                  // we read into this
+  
 
   char string[DECNUMDIGITS+14];    // conversion buffer
   int  i, j, numbers;              // counters
@@ -60,7 +61,8 @@ int main(int argc, char *argv[]) {
   int  tax=1;                      // 1 for tax calculations, 0 to skip
  // struct timeb tbStart, tbFinis;   // time buffers
  //////  struct clock_t tbStart, tbFinis;   // time buffers
-   unsigned long  totalTime;//cyclestart,cycleend,cyclestart_ex,cycleend_ex; // declare for clock test
+  char inputNumber[20]; 
+  unsigned long  totalTime;//cyclestart,cycleend,cyclestart_ex,cycleend_ex; // declare for clock test
    clock_t timeStart = 0;
    clock_t timeEnd   = 0;
 
@@ -68,19 +70,19 @@ int main(int argc, char *argv[]) {
   char **parm;                     // ..
 
   // set up arithmetic context
-  decContextDefault(&set, DEC_INIT_DECIMAL128);   // initialize
+ //# decContextDefault(&set, DEC_INIT_DECIMAL128);   // initialize
   
   decContextDefault(&set, DEC_INIT_DECDOUBLE);   // initialize 64 bit operation
  
-  set.traps=0;                                    // no traps
-  set.digits=31;                                  // working precision
+ //# set.traps=0;                                    // no traps
+ //# set.digits=31;                                  // working precision
 
   // set up the call rates, tax rates and other constants
-  decNumberFromString(&baserate, "0.0013", &set); // low call rate
-  decNumberFromString(&distrate, "0.00894", &set);// high call rate
-  decNumberFromString(&basetax,  "0.0675", &set); // base tax rate
-  decNumberFromString(&disttax,  "0.0341", &set); // distance tax rate
-  decNumberFromString(&mtwo,     "-2", &set);     // scale setting
+ //# decNumberFromString(&baserate, "0.0013", &set); // low call rate
+ //# decNumberFromString(&distrate, "0.00894", &set);// high call rate
+ //# decNumberFromString(&basetax,  "0.0675", &set); // base tax rate
+ //# decNumberFromString(&disttax,  "0.0341", &set); // distance tax rate
+ //# decNumberFromString(&mtwo,     "-2", &set);     // scale setting
 
 
   // set up 64-bit format the call rates, tax rates and other constants
@@ -92,15 +94,16 @@ int main(int argc, char *argv[]) {
 
 
 
-  printf("new double telco");
+ // printf("new double telco");
 
   // Get any parameters
-  strcpy(filein, "telco.testr");             // default filenames
-  strcpy(fileou, "telco.outc");              // ..
+ // strcpy(filein, "telco.testr");             // default filenames expon180.1e6r
+ strcpy(filein, "expon180.1e6r");  
+strcpy(fileou, "telco.outc");              // ..
 
-  j=0;                                       // parameter number
+  j=0;                                       //counter( parameter number
   for (i=0, parm=argv; i<argc ; parm++, i++) {
-  printf("riaz itaration start");
+ // printf("riaz itaration start");
 
     if (strlen(*parm)>MAXFILE) {             // no buffer overruns, please
       printf("Parameter word too long (>%d characters): %s\n", MAXFILE, *parm);
@@ -118,7 +121,7 @@ int main(int argc, char *argv[]) {
       else printf("Extra parameter '%s' ignored\n", *parm);
     }
 
- printf ("value of the bianry : %s\n", filein);
+// printf ("value of the bianry : %s\n", filein);
 
   } /* getting arguments */
 
@@ -130,6 +133,9 @@ int main(int argc, char *argv[]) {
 
   remove(fileou);                  // delete output file if it exists
 
+
+ //  for (int ami=0; ami <99 ; ami++) {
+
   /* ---------------------------------------------------------------- */
   /* Benchmark timing starts here                                     */
   /* ---------------------------------------------------------------- */
@@ -137,10 +143,18 @@ int main(int argc, char *argv[]) {
 
   timeStart = clock(); //Start count Time comment for Cycle cout
 
- // decDoubleZero(&sumT);
-  decNumberZero(&sumT);            // zero accumulators
-  decNumberZero(&sumB);
-  decNumberZero(&sumD);
+ 
+ //# decNumberZero(&sumT);            // zero accumulators
+ //# decNumberZero(&sumB);
+ //# decNumberZero(&sumD);
+
+
+  decDoubleZero(&d_sumT);            // zero accumulators
+  decDoubleZero(&d_sumB);
+  decDoubleZero(&d_sumD);
+
+
+
 
   inp=fopen(filein,"rb");          // open file for reading, binary
   if (inp==NULL) {
@@ -159,7 +173,7 @@ int main(int argc, char *argv[]) {
   for (numbers=0; ; numbers++) {
     // get next 8-byte decimal64 number
     readlen=fread(in64.bytes, 1, DECIMAL64_Bytes, inp);
-    printf("riaz readlen value  \n %d",readlen);
+   // printf("riaz readlen value  \n %d",readlen);
 
     
     if (readlen<DECIMAL64_Bytes) {      // error or EOF
@@ -170,62 +184,63 @@ int main(int argc, char *argv[]) {
     }
     if (calc!=0) {
       calltype=((unsigned)in64.bytes[DECIMAL64_Bytes-1]) & 0x01; // last bit
-      printf("riaz call type is not zero  and is %d\n" , calltype);
+     // printf("riaz call type is not zero  and is %d\n" , calltype);
       // have packed decimal number; convert to working format
-     /////// decimal64ToNumber(&in64, &n);
+      //# decimal64ToNumber(&in64, &n);
+       decimal64ToString(&in64, inputNumber);
+       decDoubleFromString(&d_n, inputNumber, &set); // low call rate
 
+//riaz
       if (calltype==0) {                                // p=r[c]*n
-        decNumberMultiply(&p, &baserate, &n, &set);
-      //  decDoubleMultiply(&d_p, &d_baserate, &d_n, &set);
+       //# decNumberMultiply(&p, &baserate, &n, &set);
+         decDoubleMultiply(&d_p, &d_baserate, &d_n, &set);
                        } 
       else {
-      decNumberMultiply(&p, &distrate, &n, &set);
-      //decDoubleMultiply(&d_p, &d_distrate, &d_n, &set);
+      //#decNumberMultiply(&p, &distrate, &n, &set);
+       decDoubleMultiply(&d_p, &d_distrate, &d_n, &set);
           }
-      set.round=DEC_ROUND_HALF_EVEN;                   // round prices
+     //# set.round=DEC_ROUND_HALF_EVEN;                   // round prices
      // decNumberRescale(&p, &p, &mtwo, &set);           // to x.xx
 
       if (tax!=0) {
         set.round=DEC_ROUND_DOWN;                      // truncate taxes
-        decNumberMultiply(&b, &p, &basetax, &set);     // b=p*0.0675
-       // decDoubleMultiply(&d_b, &d_p, &d_basetax, &set); //double b=p*0.0675
+       //# decNumberMultiply(&b, &p, &basetax, &set);     // b=p*0.0675
+        decDoubleMultiply(&d_b, &d_p, &d_basetax, &set); //double b=p*0.0675
 
-        decNumberRescale(&b, &b, &mtwo, &set);         // to x.xx
+       //# decNumberRescale(&b, &b, &mtwo, &set);         // to x.xx
         
-        decNumberAdd(&sumB, &sumB, &b, &set);          // sumB=sumB+b
-        decNumberAdd(&t, &p, &b, &set);                // t=p+b
+       //# decNumberAdd(&sumB, &sumB, &b, &set);          // sumB=sumB+b
+       //# decNumberAdd(&t, &p, &b, &set);                // t=p+b
                     
         decDoubleAdd(&d_sumB, &d_sumB, &d_b, &set);          // sumB=sumB+b
         decDoubleAdd(&d_t, &d_p, &d_b, &set);          
 
         
         if (calltype!=0) {
-          decNumberMultiply(&d, &p, &disttax, &set);   // d=p*0.0341
-          decNumberRescale(&d, &d, &mtwo, &set);       // to x.xx
-          decNumberAdd(&sumD, &sumD, &d, &set);        // sumD=sumD+d
-          decNumberAdd(&t, &t, &d, &set);              // t=t+d
+         //# decNumberMultiply(&d, &p, &disttax, &set);   // d=p*0.0341
+         //# decNumberRescale(&d, &d, &mtwo, &set);       // to x.xx
+         //# decNumberAdd(&sumD, &sumD, &d, &set);        // sumD=sumD+d
+         //# decNumberAdd(&t, &t, &d, &set);              // t=t+d
 
            
 
           decDoubleMultiply(&d_d, &d_p, &d_disttax, &set);   // d=p*0.0341
-          //decNumberRescale(&d, &d, &mtwo, &set);       // to x.xx
           decDoubleAdd(&d_sumD, &d_sumD, &d_d, &set);        // sumD=sumD+d
           decDoubleAdd(&d_t, &d_t, &d_d, &set);              // t=t+d
 
-
-  
         }
       } else {
-        t=p;
+       //# t=p;
         d_t = d_p;
       }
-      decNumberAdd(&sumT, &sumT, &t, &set);            // sumT=sumT+t
-      decNumberToString(&t, string);
-      printf("[%d] %d: %s\n", calltype, numbers, string);
+     //# decNumberAdd(&sumT, &sumT, &t, &set);            // sumT=sumT+t
+     //# decNumberToString(&t, string);
+     //# printf("[%d] %d: %s\n", calltype, numbers, string);
 
 
       decDoubleAdd(&d_sumT, &d_sumT, &d_t, &set);            // sumT=sumT+t
       decDoubleToString(&d_t, string);
+       printf("[%d] %d: %s\n", calltype, numbers, string);
 
 
     } else {
@@ -261,30 +276,33 @@ int main(int argc, char *argv[]) {
   printf("-- telco C benchmark result --\n");
   printf("   %d numbers read from '%s'\n", numbers, filein);
 //  showus("Time per number", tbStart, tbFinis, numbers);
-   printf("Took %lu cycles to Original Library \n", totalTime);
+   printf("Took %lu second \n", totalTime);
 
-  printf("--\n");
-  decNumberToString(&sumT, string);
-  printf("   sumT = %s\n", string);
-  decNumberToString(&sumB, string);
-  printf("   sumB = %s\n", string);
-  decNumberToString(&sumD, string);
-  printf("   sumD = %s\n", string);
+ //# printf("--\n");
+ //# decNumberToString(&sumT, string);
+ //# printf("   sumT = %s\n", string);
+ //# decNumberToString(&sumB, string);
+ //# printf("   sumB = %s\n", string);
+ //# decNumberToString(&sumD, string);
+ //# printf("   sumD = %s\n", string);
 
-  decNumberToString(&n, string);
-  printf("   n = %s\n", string);
+ //# decNumberToString(&n, string);
+ //# printf("   n = %s\n", string);
 
 
 
 
   printf("- 64 bit format-\n");
   decDoubleToString(&d_sumT, string);
-  printf("   sumT = %s\n", string);
+  printf("  double sumT = %s\n", string);
   decDoubleToString(&d_sumB, string);
-  printf("   sumB = %d\n", string);
+  printf("  double sumB = %s\n", string);
   decDoubleToString(&d_sumD, string);
-  printf("   sumD = %s\n", string);
+  printf("  double sumD = %s\n", string);
 
+
+ decDoubleToString(&d_n, string);
+ printf("   n = %s\n", string);
 
 
 
